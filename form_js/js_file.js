45c2214aab92fd16161d8c5c24590c02b9f93d36
Randheerrrk <Randheerrrk@gmail.com>
2020-07-23 00:07:00 +0530

    UI changes and added functions

--- form_js/js_file.js
+++ form_js/js_file.js
@@ -1,24 +1,30 @@
-var form = document.getElementById("form");
+const form = document.forms[0]
+const headings = [ "fname", "lname", "address", "age", "gender", "interest"]
 form.addEventListener('submit', (event)=> {
     event.preventDefault()
-    var interests=""
-    var fname = document.getElementById("fname").value;
-    var lname = document.getElementById("lname").value;
-    var address = document.getElementById("address").value;
-    var age = document.getElementById("age").value;
-    var gender = document.getElementById("genderSelect").value;
-    var interest = document.getElementsByName("interest");
-    for(var i=0; i<interest.length; i++) {
-        if(interest[i].checked) {
-            interests+=interest[i].value+' ';
+    var formdata = new FormData(form)
+    createComp(formdata)
+})
+
+createComp = (formdata) => {
+    var table = document.getElementsByTagName("table")[0]
+    var newRow = table.insertRow(table.rows.length)
+    for(var i=0; i<table.rows[0].cells.length; i++) {
+        if(headings[i]==="interest") {
+            var interestChecked=""
+            for(var j=0; j<4; j++) {
+                if(formdata.get("interest"+j)!==null) {
+                    interestChecked+=formdata.get("interest"+j)+"<br>"
+                }
+            } 
+            newRow.insertCell(i).innerHTML = interestChecked
+        }
+        else if(headings[i]==="address") {
+            newRow.insertCell(i).innerHTML = formdata.get('address').replace(/\r?\n/g, "<br>")
+        }    
+        else {
+            newRow.insertCell(i).innerHTML = formdata.get(headings[i])
         }
     }
-
-
-    document.getElementById("resfname").innerHTML = "First Name: "+ fname;
-    document.getElementById("reslname").innerHTML = "Last Name: "+ lname;
-    document.getElementById("resaddress").innerHTML = "Address: "+ address;
-    document.getElementById("resage").innerHTML = "Age: "+ age;
-    document.getElementById("resgender").innerHTML = "Gender: "+ gender;
-    document.getElementById("resinterest").innerHTML = "Interest: "+ interests;
-})
+    console.log(formdata.get("interest"))
+}
