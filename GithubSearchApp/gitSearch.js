0ab1e78b4f85b40d3ed1dd5abfc2539359efe46e
Randheerrrk <Randheerrrk@gmail.com>
2020-07-27 22:19:51 +0530

    added next/prev

--- GithubSearchApp/gitSearch.js
+++ GithubSearchApp/gitSearch.js
@@ -1,38 +1,86 @@
-document.addEventListener('submit', (event) => {
-    event.preventDefault()
-    var form = event.target
-    return fetchData(form)
-})
-
+var form;
 const per_page = 20;
 var pageNo = 1;
 
-const fetchData = async (form) => {
-    let result = await (await fetch(`${form.action}${form.elements.searchVal.value}&page=${pageNo}&per_page=${per_page}`)).json()
-    let items = result.items
-    let resultDiv = document.getElementById("result")
-    while(resultDiv.firstChild) {
-        resultDiv.firstChild.remove()
+document.addEventListener('submit', (event) => {
+    event.preventDefault()
+    pageNo = 1
+    form = event.target
+    return fetchData(form)
+})
+
+document.addEventListener('click', (event) => {
+    if(event.target.id==="prev_page") {
+        pageNo-=1
+        fetchData(form)
     }
-    let uL = document.createElement('ul')
-    items.map((item, index) => {
-        var li = document.createElement('li')
-        var childCard = document.createElement("div")
-        childCard.className = "card"
-        let img = document.createElement("img")
-        img.src = item.avatar_url
-        childCard.appendChild(img)
-        var h4 = document.createElement("h4")
-        h4.innerHTML = item.login
-        childCard.appendChild(h4)
-        var a = document.createElement("a")
-        a.href = item.html_url
-        a.innerHTML = "view profile"
-        childCard.appendChild(a)
-        li.appendChild(childCard)
-        uL.appendChild(li)
-    })
-    resultDiv.appendChild(uL)
+    else if(event.target.id==="next_page") {
+        pageNo+=1
+        fetchData(form)
+    }
+})
+
+
+const fetchData = async (form) => {
+    let resultDiv = document.getElementById("result")
+    if(form.elements.searchVal.value!="") {
+        let result = await (await fetch(`${form.action}${form.elements.searchVal.value}&page=${pageNo}&per_page=${per_page}`)).json()
+        let items = result.items
+        while(resultDiv.firstChild) {
+            resultDiv.firstChild.remove()
+        }
+        let uL = document.createElement('ul')
+        items.map((item, index) => {
+            var li = document.createElement('li')
+            var childCard = document.createElement("div")
+            childCard.className = "card"
+            let img = document.createElement("img")
+            img.src = item.avatar_url
+            childCard.appendChild(img)
+            var h4 = document.createElement("h4")
+            h4.innerHTML = item.login
+            childCard.appendChild(h4)
+            var a = document.createElement("a")
+            a.href = item.html_url
+            a.innerHTML = "view profile"
+            childCard.appendChild(a)
+            li.appendChild(childCard)
+            uL.appendChild(li)
+        })
+        let pageDiv = document.createElement("div")
+        pageDiv.id = "pageDiv"
+        let prev_page = document.createElement("a")
+        prev_page.innerHTML = "&laquo; Previous"
+        prev_page.id = "prev_page"
+        prev_page.href = "#"
+        if(pageNo===1) {
+            prev_page.style.visibility = "hidden";
+        }
+        let page_no = document.createElement('p')
+        page_no.innerHTML = pageNo
+        page_no.id = "page_no"
+        let next_page = document.createElement("a")
+        next_page.innerHTML = "Next &raquo;"
+        next_page.id = "next_page"
+        next_page.href = "#"
+        if(items.length<per_page) {
+            next_page.style.visibility = "hidden";
+        }
+        pageDiv.appendChild(prev_page)
+        pageDiv.appendChild(page_no)
+        pageDiv.appendChild(next_page)
+        resultDiv.appendChild(uL)
+        resultDiv.appendChild(pageDiv)
+    }
+    else {
+        while(resultDiv.firstChild) {
+            resultDiv.firstChild.remove()
+        }
+        var p = document.createElement("p")
+        p.innerHTML = "Enter a name..."
+        p.id = "noValue"
+        resultDiv.appendChild(p)
+    }
 }
 
 
--- form_js/js_file.js
+++ form_js/js_file.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 document.addEventListener('submit', (event)=> {
     event.preventDefault()
     var formData = event.target.elements
@@ -26,3 +27,29 @@ createComp = (formdata) => {
         }
     }
 }
+=======
+var form = document.getElementById("form");
+form.addEventListener('submit', (event)=> {
+    event.preventDefault()
+    var interests=""
+    var fname = document.getElementById("fname").value;
+    var lname = document.getElementById("lname").value;
+    var address = document.getElementById("address").value;
+    var age = document.getElementById("age").value;
+    var gender = document.getElementById("genderSelect").value;
+    var interest = document.getElementsByName("interest");
+    for(var i=0; i<interest.length; i++) {
+        if(interest[i].checked) {
+            interests+=interest[i].value+' ';
+        }
+    }
+
+
+    document.getElementById("resfname").innerHTML = "First Name: "+ fname;
+    document.getElementById("reslname").innerHTML = "Last Name: "+ lname;
+    document.getElementById("resaddress").innerHTML = "Address: "+ address;
+    document.getElementById("resage").innerHTML = "Age: "+ age;
+    document.getElementById("resgender").innerHTML = "Gender: "+ gender;
+    document.getElementById("resinterest").innerHTML = "Interest: "+ interests;
+})
+>>>>>>> parent of 45c2214... UI changes and added functions
